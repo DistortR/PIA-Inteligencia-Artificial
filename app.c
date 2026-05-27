580629e2c2761ed0935807493e5be9d25df65a10
OvFabri <fabrizio@0112@outlook.com>
2026-05-27 14:00:12 -0600

    DFS-y-Grafo

--- app.c
+++ app.c
@@ -5,8 +5,11 @@
 #include "modular/BFS.c"
 #include "libraries/UCS.h"
 #include "modular/UCS.c"
+#include "libraries/DFS.h"
+#include "modular/DFS.c"
 
 void printOptions();
+void loadDefaultGraph(Graph *g);
 
 typedef struct Node Node;
 
@@ -14,6 +17,8 @@ int main(){
     int option=1, c;
     Graph g;
     initGraph(&g);
+    loadDefaultGraph(&g);
+    printf("Grafo predeterminado cargado.\n");
 
     while(option != 0){
         printOptions();
@@ -55,11 +60,53 @@ int main(){
                 break;
 
             case 4:
-                //UCS();
+                if (g.head == NULL) {
+                    printf("No hay grafo definido.\n");
+                    break;
+                }
+
+                char startNameUCS[50], goalNameUCS[50];
+                Node *startUCS, *goalUCS;
+
+                printf("Nodo inicio $");
+                scanf("%s", startNameUCS);
+                printf("Nodo meta $");
+                scanf("%s", goalNameUCS);
+
+                startUCS = findNode(&g, startNameUCS);
+                goalUCS  = findNode(&g, goalNameUCS);
+
+                if (startUCS == NULL)
+                    printf("Nodo '%s' no existe.\n", startNameUCS);
+                else if (goalUCS == NULL)
+                    printf("Nodo '%s' no existe.\n", goalNameUCS);
+                else
+                    UCS(&g, startUCS, goalUCS);
                 break;
 
             case 5:
-                //DFS();
+                if (g.head == NULL) {
+                    printf("No hay grafo definido.\n");
+                    break;
+                }
+
+                char startNameDFS[50], goalNameDFS[50];
+                Node *startDFS, *goalDFS;
+
+                printf("Nodo inicio $");
+                scanf("%s", startNameDFS);
+                printf("Nodo meta $");
+                scanf("%s", goalNameDFS);
+
+                startDFS = findNode(&g, startNameDFS);
+                goalDFS  = findNode(&g, goalNameDFS);
+
+                if (startDFS == NULL)
+                    printf("Nodo '%s' no existe.\n", startNameDFS);
+                else if (goalDFS == NULL)
+                    printf("Nodo '%s' no existe.\n", goalNameDFS);
+                else
+                    DFS(startDFS, goalDFS);
                 break;
 
             case 6:
@@ -98,4 +145,32 @@ void printOptions(){
     printf("9.- Busqueda A*\n");
     printf("0. Salir\n");
     printf("Opcion $");
+}
+void loadDefaultGraph(Graph *g) {
+   
+    addNode(g, "A", 10.0);
+    addNode(g, "B", 3.0);
+    addNode(g, "C", 9.0);
+    addNode(g, "D", 5.0);
+    addNode(g, "E", 3.0);
+    addNode(g, "F", 3.0);
+    addNode(g, "G", 8.0);
+    addNode(g, "H", 7.0);
+    addNode(g, "I", 0.0);
+
+    addEdge(g, "A", "B", 3.0);
+    addEdge(g, "A", "D", 3.0);
+    addEdge(g, "A", "C", 8.0);
+    addEdge(g, "B", "D", 5.0);
+    addEdge(g, "B", "E", 2.0);
+    addEdge(g, "D", "E", 3.0);
+    addEdge(g, "C", "E", 3.0);
+    addEdge(g, "C", "G", 5.0);
+    addEdge(g, "D", "C", 3.0);
+    addEdge(g, "E", "H", 3.0);
+    addEdge(g, "F", "H", 3.0);
+    addEdge(g, "F", "I", 2.0);
+    addEdge(g, "H", "I", 4.0);
+    addEdge(g, "H", "G", 2.0);
+    addEdge(g, "G", "I", 4.0);
 }
